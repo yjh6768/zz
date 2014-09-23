b21613ee9752d26588fa4797e2399f0a38dcc70c
yjh6768 <tkfrl258>
2014-09-23 17:35:01 +0900

    0923-3
    교수님 프로그래밍과목을 처음 수강하는 저로선 너무 어렵습니다ㅠㅠ

--- 0923.py
+++ 0923.py
@@ -4,5 +4,22 @@ def TimesTable(arg):
 		table.append(str(arg) + " * "+ str(j) + " = "+str(arg*j))
 	return table
 
-for i in range(0,10):
-	print TimesTable(2)[i]
+re = TimesTable(2)
+ge = TimesTable(3)
+he = TimesTable(4)
+ke = TimesTable(5)
+print re
+for r in re:
+	print r
+print ge
+for g in ge:
+	print g
+print he
+for h in he:
+	print h
+print ke
+for k in ke:
+	print k
+
+for r in re:
+	print r+'   '+g+'   '+h+'   '+k
